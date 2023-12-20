16e4a22570c115ae86bad3efd6fbd418b1d06d26
BassoMC <Bassow05@gmail.com>
2023-12-20 10:51:04 +0100

    Removed WeatherForcast code

---
+++ DB/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DB.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new List<string> { "data1", "data2", "data3" };
+        }
+    }
+}
