8bdbd8017f65cfcf1b1631f1091ed1be82f0e0b0
BassoMC <Bassow05@gmail.com>
2023-12-21 10:10:12 +0100

    Read Json file

---
+++ Commands/Read.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace Commands
+{
+    public static class Read
+    {
+        public static Dictionary<string, string> ReadJson(string JsonFilePath)
+        {
+            try
+            {
+                
+                string jsonString = File.ReadAllText(JsonFilePath);
+
+                // Deserialize the JSON string into a Dictionary
+                Dictionary<string, string> yourObject = JsonSerializer.Deserialize<Dictionary<string, string>>(jsonString);
+
+                return yourObject;
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions (e.g., file not found, deserialization issues)
+                Console.WriteLine("Error: " + ex.Message);
+                return null;
+            }
+        }
+    }
+}
--- DB/Controllers/TestController.cs
+++ DB/Controllers/TestController.cs
@@ -9,6 +9,21 @@ namespace DB.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
+            var test = Commands.Read.ReadJson("data.json");
+
+            if (test != null)
+            {
+                foreach (var pair in test)
+                {
+                    Console.WriteLine($"Key: {pair.Key}, Value: {pair.Value}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Failed to read or parse the JSON file.");
+            }
+
+
             return new List<string> { "data1", "data2", "data3", "data4" };
         }
     }
